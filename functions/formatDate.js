28064287b67a88de94e69b0e1c6b8a333fb4d9f2
yinghong <yinghong@txcombo.com>
2024-04-09 14:29:40 +0800

    优化日期显示

--- functions/formatDate.js
+++ functions/formatDate.js
@@ -1,44 +1,8 @@
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
-
-const nth = (d) => {
-  if(d>3 && d<21) return 'th';
-  switch (d % 10) {
-    case 1: return "st";
-    case 2: return "nd";
-    case 3: return "rd";
-    default: return "th";
-  }
-};
+const moment = require('moment');
 
 module.exports = (dateString) => {
   const date = new Date(dateString);
-  const day = date.getDate();
-  const dayIndex = date.getDay();
-  const monthIndex = date.getMonth();
-  const year = date.getFullYear();
-
-  return `${days[dayIndex]} ${day}${nth(day)} ${months[monthIndex]} ${year}`;
+  const m = moment(date);
+  m.locale('zh-cn');
+  return `${m.format('LLLL')} | ${m.fromNow()}`;
 };
